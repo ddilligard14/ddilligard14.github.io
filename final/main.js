a1b5e2f84d6d81d3e0e7fed4a5c608c7d02a0081
Deon <ddilligard14@gmail.com>
2016-09-22 18:19:58 +0000

    final3

--- final/main.js
+++ final/main.js
@@ -1,13 +1,7 @@
-
-
-var ccUrl= "https://www.codecademy.com/ddilligard14";
 var btnAcademy= document.getElementById("btnAcademy");
+btnAcademy.onmousedown = btnAcademy;
 
-btnAcademy.onmousedown = myCodecademy;
 
-function myCodecademy (){
-    window.open (ccUrl);
-}
 
 var btnGreet 
 function btnGreet(){
