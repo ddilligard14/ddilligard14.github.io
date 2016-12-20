74b931bfbd741d007d1809ba1ad0bcf3af8dc36c
Deon <ddilligard14@gmail.com>
2016-12-20 16:38:34 +0000

    finalmusic 2

--- finalmusic/songs.js
+++ finalmusic/songs.js
@@ -8,62 +8,54 @@ var songs = {
 		picture: "khaledMind"
 	},
 	2:{
-		song:"Elton John - Mona Lisas And Mad Hatters",
-		picture: "EltonMona"
-	},
-	3:{
-		song:"Journey - Faithfully",
-		picture: "JourneyFaith"
-	},
-	4:{
 		song:"Meek Mill - Blessed Up",
 		picture: "Blessedup"
 	},
-	5:{
+	3:{
 		song:"Migos - Bad and Boujee ft Lil Uzi Vert",
 		picture: "Migos"
 	},
-	6:{
+	4:{
 		song:"Young M.A - Ooouuu",
 		picture: "ooouuu"
 	},
-	7:{
+	5:{
 		song:"2 Chainz - Big Amount ft Drake",
 		picture: "BigAmount"
 	},
-	8:{
+	6:{
 		song:"DJ ESCO - Too Much Sauce ft Future Lil Uzi Vert",
 		picture: "Sauce"
 	},
-	9:{
+	7:{
 		song:"Big Baby D.R.A.M. - Broccoli ft Lil Yachty",
 		picture: "Broc"
 	},
-	10:{
+	8:{
 		song:"Rae Sremmurd - Black Beatles ft Gucci Mane",
 		picture: "Beetle"
 	},
-	11:{
+	9:{
 		song:"Drake - Energy",
 		picture: "Energy"
 	},
-	12:{
+	10:{
 		song:"J. Cole - Everybody Dies",
 		picture: "Cole"
 	},
-	13:{
+	11:{
 		song:"Rihanna - Needed Me",
 		picture: "Needed"
 	},
-	14:{
+	12:{
 		song:"A Boogie Wit Da Hoodie - Not A Regular Person",
 		picture: "Regular"
 	},
-	15:{
+	13:{
 		song:"Jhene Aiko - W.A.Y.S",
 		picture: "ways"
 	},
-	16:{
+	14:{
 		song:"Drake - Childs Play",
 		picture: "Childs"
 	},
