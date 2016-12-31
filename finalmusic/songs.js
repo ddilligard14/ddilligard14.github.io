e4a8e0aafdf24243e1d52cb840956b66672f0438
Deon <ddilligard14@gmail.com>
2016-12-31 18:30:47 +0000

    finalmusic1

--- finalmusic/songs.js
+++ finalmusic/songs.js
@@ -63,7 +63,92 @@ var songs = {
 		song:"The Weeknd - Starboy ft. Daft Punk",
 		picture: "star1"
 	},
-	
-	
+	16:{
+		song:"Big Sean - Blessings ft. Drake, Kanye West",
+		picture: "dsp"
+	},
+	17:{
+		song:"Big Sean - Play No Games ft. Chris Brown, Ty Dolla $ign",
+		picture: "dsp"
+	},
+	18:{
+		song:"Drake - 30 for 30 Freestyle",
+		picture: "wattba"
+	},
+	19:{
+		song:"Drake & Future - Big Rings",
+		picture: "wattba"
+	},
+	20:{
+		song:"Drake & Future - Change Locations",
+		picture: "wattba"
+	},
+	21:{
+		song:"Drake & Future - Diamonds Dancing",
+		picture: "wattba"
+	},
+	22:{
+		song:"Drake & Future - Digital Dash",
+		picture: "wattba"
+	},
+	23:{
+		song:"Drake & Future - I'm the Plug",
+		picture: "wattba"
+	},
+	24:{
+		song:"Drake & Future - Jumpman",
+		picture: "wattba"
+	},
+	25:{
+		song:"Drake & Future - Live From the Gutter",
+		picture: "wattba"
+	},
+	26:{
+		song:"Drake & Future - Plastic Bag",
+		picture: "wattba"
+	},
+	27:{
+		song:"Drake & Future - Scholarships",
+		picture: "wattba"
+	},
+	28:{
+		song:"Future - Jersey",
+		picture: "wattba"
+	},
+	29:{
+		song:"PARTYNEXTDOOR - Come and See Me",
+		picture: "pndcs"
+	},
+	30:{
+		song:"PARTYNEXTDOOR - Recognize ft. Drake",
+		picture: "rec"
+	},
+	31:{
+		song:"Rae Sremmurd - Swang",
+		picture: "Beetle"
+	},32:{
+		song:"Rae Sremmurd - This Could Be Us",
+		picture: "slrs"
+	},
+	33:{
+		song:"Travis Scott - Antidote",
+		picture: "trant"
+	},
+	34:{
+		song:"",
+		picture: ""
+	},
+	35:{
+		song:"",
+		picture: ""
+	},
+	36:{
+		song:"",
+		picture: ""
+	},
+	37:{
+		song:"",
+		picture: ""
+	},
 };
 
